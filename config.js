e00dc8e4d68ac7ba667994251b2b9e660ffddeb7
4k29 <2hjpkx4564@privaterelay.appleid.com>
2026-07-25 17:40:42 +0900

    Add service configuration

---
+++ config.js
@@ -0,0 +1,5 @@
+export const SUPABASE_URL = "YOUR_SUPABASE_URL";
+export const SUPABASE_ANON_KEY = "YOUR_SUPABASE_ANON_KEY";
+
+export const APP_NAME = "Travel Ticket";
+export const MAX_PHOTOS_PER_DAY = 9;
